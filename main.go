6b9b54296fd95870d7164a9df3b47d9930ba195a
NATARAJAN @. KUMAR N C KRISHNAN <rajan@my.ibm.com>
2020-06-03 14:03:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 import (
         "net/http"
+    	"log"
         "fmt"
         "math/rand"
         "github.com/prometheus/client_golang/prometheus"
@@ -16,8 +17,10 @@ var (
 )
 
 func main() {
+    log.Printf("main function")
     http.HandleFunc("/", handle)
     http.Handle("/metrics", promhttp.Handler())
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
